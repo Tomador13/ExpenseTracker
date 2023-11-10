1428869a8641a67a2335ff1c584a7dd2b90ab874
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2023-11-10 15:16:02 +0800

    zz

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react"
 const About = () => {
+    const [ balance , setBalance ] = useState("")
+
+    const handleClick = () => {
+
+    }
+    
     return <>
         <section>
             <h1>About Me</h1>
-            <input type="text" /><button>Enter Money</button>
+            <h2>Balance: { balance } </h2>
+            <input type="text" value = {balance} onChange = { (e) => {setBalance( e.event.target )}} /><button onClick = { handleClick }>Enter Money</button>
         </section>
     </>
 }
