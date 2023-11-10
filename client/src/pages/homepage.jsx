90aaf3ae374351886e48297b1762dffc1c6eaa27
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2023-11-10 16:57:30 +0800

    zz

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,52 +1,59 @@
 import { useState } from "react";
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
 const HomePage = () => {
     const location = useLocation();
-    const initialMoney = location.state ? location.state.initialMoney : 0 ; 
-    const [ money , setMoney ] = useState( initialMoney )
-    const [ initialName , setInitialName ] = useState( 'You' )
-    const [ name , setName ] = useState('')
-    const [ price , setPrice ] = useState()
-    const [ list , setList ] = useState([])
+    const initialMoney = location.state ? location.state.initialMoney : 0; 
+    const [money, setMoney] = useState(initialMoney);
+    const [initialName, setInitialName] = useState('You');
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [list, setList] = useState([]);
 
     const handleClick = () => {
-        money < price ? setInitialName ( 'Not Enough Balance' ) : setMoney ( money - price ) ;
-        setList([ ...list , name ])
-        setName('')
-        setPrice('')
-        console.log(...list)
-    }
-    
+        money < price
+            ? setInitialName('Not Enough Balance')
+            : setMoney(money - price);
+        setList([...list, name]);
+        setName('');
+        setPrice('');
+        console.log(...list);
+    };
+
     const goto = () => {
-        window.location = '/about'
-    }
-    return <>
+        window.location = '/about';
+    };
+
+    return (
         <section className="homepage">
             <div>
-                <h1> Expense Tracker for { initialName } </h1>
-                <h1 onClick = { goto } >Balance: { money }</h1>
-                    <input type="text" 
-                        placeholder = "Item Name"
-                        value = { name }
-                        onChange = { (e) => { setName( e.target.value ) } }
-                    />
-                    <input type="text" 
-                        placeholder="Price"
-                        value = { price }
-                        onChange = { (e) => { setPrice ( e.target.value ) } }
-                    />
-                <button onClick={ handleClick }>Submit</button>
+                <h1>Expense Tracker for {initialName}</h1>
+                <h1 onClick={goto}>Balance: {money}</h1>
+                <input
+                    type="text"
+                    placeholder="Item Name"
+                    value={name}
+                    onChange={(e) => {
+                        setName(e.target.value);
+                    }}
+                />
+                <input
+                    type="text"
+                    placeholder="Price"
+                    value={price}
+                    onChange={(e) => {
+                        setPrice(e.target.value);
+                    }}
+                />
+                <button onClick={handleClick}>Submit</button>
             </div>
-                <ol>
-                    { list.map( ( e , index ) => (
-                        <li>{ e }</li>
-                    ))}
-                </ol>
-
+            <ol>
+                {list.map((e, index) => (
+                    <li key={index}>{e}</li>
+                ))}
+            </ol>
         </section>
-    </>
+    );
+};
 
-}
-
-export default HomePage
+export default HomePage;
