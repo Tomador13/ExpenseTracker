599f8adb484dbca3b76236f38b4976b6597015d8
Paulo13 <107038071+Tomador13@users.noreply.github.com>
2023-11-10 16:18:55 +0800

    zz

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
-import Homepage from "/homepage"
 
 const About = () => {
     const [ balance , setBalance ] = useState(0)
